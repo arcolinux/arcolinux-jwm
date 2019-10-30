db6b554a3832c747acc1941c70d5446333567445
Erik Dubois <erik.dubois@gmail.com>
2019-10-30 06:10:04 +0100

    typo

--- etc/skel/.config/jwm/autostart.sh
+++ etc/skel/.config/jwm/autostart.sh
@@ -17,7 +17,7 @@ feh --bg-scale /usr/share/backgrounds/arcolinux/escape-to-italy-1920x1200.jpg &
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
 xsetroot -cursor_name left_ptr &
-sxhkd ~/.config/jwm/sxhkd/sxhkrc &
+sxhkd ~/.config/jwm/sxhkd/sxhkdrc &
 run variety &
 run nm-applet &
 run pamac-tray &
