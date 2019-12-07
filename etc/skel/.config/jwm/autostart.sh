1d59beb91ba694e58273d2d588a8488694556a36
Erik Dubois <erik.dubois@gmail.com>
2019-12-07 20:18:43 +0100

    compton to picom

--- etc/skel/.config/jwm/autostart.sh
+++ etc/skel/.config/jwm/autostart.sh
@@ -24,7 +24,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-compton --config $HOME/.config/jwm/compton.conf &
+picom --config $HOME/.config/jwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 #nitrogen --restore &
